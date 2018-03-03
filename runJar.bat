1084ad40fb1198df8ebb384c24a63e962163ada0
rustam1987 <rustamyuldoshev1987@gmail.com>
2018-03-03 12:36:27 -0500

    first

---
+++ runJar.bat
@@ -0,0 +1,1 @@
+start WellDotFeleniumFramework-0.0.1-SNAPSHOT.jar
---
+++ src/main/java/com/well/SeleniumFramework/Utils/ExecutionDriver.java
@@ -0,0 +1,17 @@
+package com.well.SeleniumFramework.Utils;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ExecutionDriver {
+
+	public static void main(String[] args) {
+		SeleniumExecutor exec=new SeleniumExecutor();
+		TestNGExecutor tngExec=new TestNGExecutor();
+		List<TestNGModuleDTO> list=exec.getRunnableTestCases();
+		HashMap<String,List<TestNGModuleDTO>> map_Suite=new HashMap<String,List<TestNGModuleDTO>>();
+		map_Suite.put(String.valueOf(1), list);
+		tngExec.buildXML(map_Suite);
+	
+	}
+}
